3e4a3a8d0d43d6520c301c550894d221c41b5bdc
diegomariani <diego.mariani@mail.com>
2017-05-20 20:46:35 +0100

    updated recommendation part

--- src/main/java/com/djm/tinder/App.java
+++ src/main/java/com/djm/tinder/App.java
@@ -6,7 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         try {
-            Tinder tinder = Tinder.fromAccessToken("EAAGm0PX4ZCpsBAEMjrlNKeYlY4ES1C9vRLEzzmVctgn3NrEg1l5XKyYzpm7sInsv06Su3P9pIbiBvbtZCk443VdZCamswOA2JEXJQ0x1E8bZB6ZBWQpZAjty0rN0JRQg0y0rdP8vcjZA18busWBX06vA7gaqREy1Wp1I4HzvwtkGyGhzQurzjjSpFZBiI8ZAO6GYrhEB5UEjGVohydMV5d7ZBhed8qLmnl9LEZD");
+            Tinder tinder = Tinder.fromAccessToken("");
             for (Recommendation r : tinder.getRecommendations()) {
                 System.out.println(r.getName() + " " + r.getBirthDate());
             }
